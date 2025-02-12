a37a403390b5b63da7cc39693cf8731e09350410
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-02-12 15:21:54 +0530

    Update and rename index.hjs to index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-require('./main');
+require('./main')
 app.get('/', (req, res) => {
     res.send('<h1>Hi 👋</h1>');
 });
