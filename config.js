53560574152ba500e0f39d79bc70943297d65cd7
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-02-12 17:23:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     mongoURI: 'mongodb+srv://y45uaqull3fp:y45uaqull3fp@cluster0.2mlmp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     freeChannelID: '-1002422757094',   // Channel ID for free users
     premiumChannelID: '-1002406050801', // Channel ID for premium users
-    adminID:"6140468904",
+    adminID:"6660736046",
  
   mongoVideoURI:"mongodb+srv://Hacksrvkyt:Hacksrvkyt@cluster0.ag7ba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 };
