b00307853519f5524ed88f365d4c6f441e7912bd
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-02-13 14:38:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-require('./main')
+
+// Import and execute main.js
+require('./main');
+
 app.get('/', (req, res) => {
     res.send('<h1>Hi 👋</h1>');
 });
 
+// Start server
 app.listen(port, () => {
     console.log(`✅ Server running at http://localhost:${port}`);
 });
