90898e394c0b167546d856aa366c0babc1fb77c6
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-02-13 16:21:48 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,14 +7,6 @@ cron.schedule('0 * * * *', async () => {
     await checkExpiredSubscriptions();
 });
 // Initialize the bot instance here
-const bot = new TelegramBot(config.token, { webHook: true });
-
-bot.setWebHook(config.WEBHOOK_URL, { max_connections: 40 })
-    .then(() => {
-        console.log('✅ Webhook set successfully');
-    })
-    .catch((error) => {
-        console.error('❌ Failed to set webhook:', error);
-    });
+const bot = new TelegramBot(config.token, { polling: true });
 
 module.exports = bot;  // Export the bot instance to be used in other files
